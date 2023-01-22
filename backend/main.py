b406be7e16ebd5cd96a53737e26e6715bf47ae60
parnavh <parnav100sach@gmail.com>
2023-01-22 06:00:16 +0530

    hmmmm
    Co-authored-by: Rushabh Javeri <javeri.rushabh45@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -11,32 +11,40 @@ chrome_options.headless = True
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
+
 def main(query: str):
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get(f"https://www.google.com/search?q={quote(query)}+stocks&tbm=nws")
-
-    content = driver.find_elements(
-        By.XPATH, '//*[@id="search"]/div/div/div/div/div/div/div/a'
-    )
-
     articles_data = []
 
-    for x in content:
-        article = Article(x.get_attribute("href"))
-        try:
-            article.download()
-        except:
+    for x in range(0, 2):
+        driver.get(
+            f"https://www.google.com/search?q={quote(query)}+stocks&tbm=nws&start={x*10}")
+
+        content = driver.find_elements(
+            By.XPATH, '//*[@id="search"]/div/div/div/div/div/div/div/a'
+        )
+
+        for x in content:
+            author = x.find_element(By.XPATH, ".//descendant::span[1]")
+            time = x.find_element(By.XPATH, ".//descendant::span[3]")
+            article = Article(x.get_attribute("href"))
             try:
                 article.download()
+                article.parse()
             except:
-                continue
+                try:
+                    article.download()
+                    article.parse()
+                except:
+                    continue
 
-        article.parse()
-        if not article.meta_description:
-            continue
-        articles_data.append({ "description": article.meta_description, "title": article.title, "url": article.url })
+            if not article.meta_description:
+                continue
+            articles_data.append({"author": author.text, "description": article.meta_description,
+                                  "title": article.title, "url": article.url, "text": article.text, "time": time.text})
 
     return articles_data
+
 
 if __name__ == "__main__":
     print(main("reliance"))
---
+++ main.py
@@ -0,0 +1,89 @@
+import streamlit as st
+from pandas_datareader import data as pdr
+import yfinance as yf
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+from utils import get_start_date, get_effect_on_trend, get_search_term, sanitize_text, colorize
+import backend.main as scraper
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
+tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
+model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
+classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+
+yf.pdr_override()
+
+st.title("Stock Sentiment Analysis")
+st.text(
+    "Predicting stock trend based on recent news and social media posts")
+
+col1, col2 = st.columns(2)
+
+def on_change():
+    st.session_state.stock = st.session_state.stock.upper()
+
+with col1:
+    st.text_input(label="Enter Stock Ticker", key="stock", value="AMZN", on_change=on_change)
+
+with col2:
+    option = st.selectbox('Choose Chart Type',
+                          ('Candlestick chart', 'Line chart'))
+
+df = pdr.get_data_yahoo(st.session_state.stock, start=get_start_date())
+
+if option == "Candlestick chart":
+    fig = ff.create_candlestick(
+        dates=df.index, open=df['Open'], close=df['Close'], high=df['High'], low=df['Low'])
+else:
+    fig = go.Figure([go.Line(y=df['High'], x=df.index)])
+
+fig.update_layout(title="Interactive Chart", xaxis_title="Time", yaxis_title="Share Price",
+                  xaxis_rangeslider_visible=True)
+
+st.plotly_chart(fig, use_container_width=True, theme="streamlit")
+
+st.subheader("News Sentiment Analysis")
+
+news_sentiment = """
+| News Article | Publisher | Publish Time &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | Sentiment | Effect on Trend |
+| ------------ | --------- | ------------ | --------- | --------------- |
+"""
+
+@st.cache(ttl=3600, show_spinner=False)
+def get_news():
+    return scraper.main(get_search_term(st.session_state.stock))
+
+with st.spinner(text="Analyzing news articles..."):
+    search_res = get_news()
+
+stock_score = 0
+neutral_count = 0
+
+classifications = []
+
+for x in search_res:
+    res = classifier(f"{sanitize_text(x['title'])}. {sanitize_text(x['description'][:600])}")[0]
+    classifications.append(res)
+
+    if res['label'] == "negative":
+        stock_score -= res['score']
+    elif res['label'] == "positive":
+        stock_score += res['score']
+    else:
+        neutral_count += 1
+
+    news_sentiment += f'| [{sanitize_text(x["title"])}]({x["url"]}) | *{x["author"]}* | {x["time"]} | **{colorize(res["label"], res["score"])}** | {get_effect_on_trend(res["label"])} |\n'
+
+if neutral_count/len(classifications) > 0.5:
+    st.error("Stock trend is likely to remain stable")
+else:
+    if stock_score > 0.5:
+        st.success("Stock is likely to go up")
+    elif stock_score < -0.5:
+        st.error("Stock is likely to go down")
+    else:
+        st.warning("Stock trend is likely to remain stable")
+
+st.markdown(news_sentiment)
+
